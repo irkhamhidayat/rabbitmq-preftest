b64b9a793a08407af8f2236eb5c6b346ae4d3e8f
irkhamdayat11 <111201912362@mhs.dinus.ac.id>
2023-03-16 17:56:28 +0700

    update index

--- exec.sh
+++ exec.sh
@@ -1,1 +1,1 @@
-./rabbitmq-test/bin/runjava com.rabbitmq.perf.PerfTest --producers 1 --consumers 1 --predeclared --routing-key rk --queue tesrabbit --use-millis -h amqp://irkhamdayat:wkwk10hehe@178.128.98.101:5672 -c 1 -C 100000 --size 100000
+./rabbitmq-test/bin/runjava com.rabbitmq.perf.PerfTest --producers 1 --consumers 0 --predeclared --routing-key rk --queue tesrabbit --use-millis -h amqp://irkhamdayat:wkwk10hehe@178.128.98.101 -c 1 -C 100 --size 100
